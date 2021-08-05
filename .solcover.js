5027fa14e88fef03ae04b9f5eb2bc68d0d19674d
Auryn Macmillan <hi@auryn.xyz>
2021-08-05 00:01:40 -0400

    Fix cocerage

--- .solcover.js
+++ .solcover.js
@@ -1,7 +1,6 @@
 module.exports = {
-    skipFiles: ['test/Imports.sol', 'test/TestExecutor.sol'],
-    mocha: {
-        grep: "@skip-on-coverage", // Find everything with this tag
-        invert: true               // Run the grep's inverse set.
-    }
+  mocha: {
+    grep: "@skip-on-coverage", // Find everything with this tag
+    invert: true, // Run the grep's inverse set.
+  },
 };
